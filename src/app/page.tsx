736c33f7cf7fc899d3393cc7a1447b6e3213976b
Semi-koron <6jimoto6@gmail.com>
2024-06-16 21:13:58 +0900

    add xr function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
     // メッシュを生成する
-    const box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+    const box = BABYLON.MeshBuilder.CreateBox("box", { size: 0.1 }, scene);
     box.position.y = 1;
     // ライトを生成する
     const light = new BABYLON.HemisphericLight(
@@ -34,6 +34,35 @@ export default function Home() {
       new BABYLON.Vector3(0, 1, 0),
       scene
     );
+    // XRエクスペリエンスを生成する
+    const xr = await scene.createDefaultXRExperienceAsync({
+      uiOptions: {
+        sessionMode: "immersive-ar",
+      },
+    });
+    // const fm = xr.baseExperience.featuresManager;
+    // const imageTracking = fm.enableFeature(
+    //   BABYLON.WebXRFeatureName.IMAGE_TRACKING,
+    //   "latest",
+    //   {
+    //     images: [
+    //       {
+    //         //URL to the image that is being tracked (you need to change it with your image, and update its real world width in meters)
+    //         src: "https://thomlucc.github.io/Assets/MarsDemo/beach.png",
+    //         estimatedRealWorldWidth: 0.15,
+    //       },
+    //     ],
+    //   }
+    // ) as BABYLON.WebXRImageTracking;
+    // //画像が見つかったらboxを表示する
+    // imageTracking.onTrackableImageFoundObservable.add((event) => {
+    //   // メッシュを生成し、トラッカー画像の位置に配置する
+    //   const trackedBox = BABYLON.MeshBuilder.CreateBox(
+    //     "trackedBox",
+    //     { size: 0.1 },
+    //     scene
+    //   );
+    // });
     // レンダリングする
     engine.runRenderLoop(() => {
       scene.render();
